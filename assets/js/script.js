2ee2be2c958eb215c683753bbe248c4c3bd138e2
GamerCleanVic <jonassd2000@gmail.com>
2023-08-04 20:47:55 -0500

    createElement

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,16 @@
 //Eventos clique2
 function clicou(){
     const teste = document.querySelector("#teste1");
-    
+    const ul = teste1.querySelector('ul');
+
+    let newUl = document.createElement('ul');
+    for(let i = 1; i <= 5; i++){
+        let newLi = document.createElement('li');
+        newLi.innerHTML = "Item add";
+        newUl.append(newLi);
+    }
+
+    ul.after(newUl);
 }
 
 /*
