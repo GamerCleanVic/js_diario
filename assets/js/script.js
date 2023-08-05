7552fab1c9bd59734b6d6064461514c6cd78d3b4
GamerCleanVic <jonassd2000@gmail.com>
2023-08-04 19:53:54 -0500

    inicial

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+//Eventos clique2
+function clicou(){
+    const teste = document.querySelector("#teste1");
+    
+}
+
+/*
+->3 períodos -> JavaScript Vanilla velocidade 2x
+->De manhã até de tarde Node (NodeJS + SQL + Express) + React.
+->De manhã até de tarde Node (NodeJS + SQL + Express) + Next.
+->De manhã até de tarde Node (NodeJS + SQL + Express) + Vue.
+->De manhã até de tarde Node (NodeJS + SQL + Express) + Angular.
+*/
+
