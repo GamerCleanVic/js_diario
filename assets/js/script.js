7403a42dc9e96e28d1f9c74d814c97a53895ca82
GamerCleanVic <jonassd2000@gmail.com>
2023-08-13 22:37:01 -0500

    herança1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,30 @@
-//Orientação a objetos em JS
-//Classes: Costructors e This
-class Person {
+//POO: Herança
 
+class Person{
     age = 0;
 
     constructor(name){
         this.name = name;
     }
+
+    sayHi() {
+        console.log(`${this.name} diz: oi!`);
+    }
 }
 
-let p1 = new Person("João");
-let p2 = new Person("Maria");
-let p3 = new Person("Pedro");
+class Student extends Person{
+    constructor(name, id){
+        super(name);
+        this.id = id;
+    }
 
-p1.age = 20;
+    sayHi(){
+        console.log(`${this.name} diz agora: olá!`);
+    }
+}
 
-console.log(`P1 = ${p1.name} tem ${p1.age} anos`);
-console.log(`P2 = ${p2.name} tem ${p2.age} anos`);
-console.log(`P3 = ${p3.name} tem ${p3.age} anos`);
+let p1 = new Student("Jotta", 1);
+
+console.log(`${p1.name} tem ${p1.age} e matrícula #${p1.id}`);
+p1.sayHi();
 
