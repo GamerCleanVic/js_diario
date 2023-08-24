53132cb54376e25aa88270a23d0c70dd5ff8e927
GamerCleanVic <jonassd2000@gmail.com>
2023-08-23 19:59:44 -0500

    callback

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,6 @@
-//Pogramação Funcional: Herança
+//Pogramação Funcional: Callback
 
-const defaultUser = {
-    name: '',
-    email: '',
-    level: 1
-}
-
-let user1 = {
-    ...defaultUser,
-    name: 'Jotta',
-    email: 'emailteste@contato.com'
-}
-
-let admin1 = {
-    name: 'CEO1',
-    email: 'qualqueradmin@teste.com',
-    level: 2
-}
-
-console.log(user1, admin1);
+document.querySelector("#botao")
+    .addEventListener('click', () => {
+        alert("Botão clicado!");
+});
