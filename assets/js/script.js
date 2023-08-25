7fb7e9ace56d1df3262ef03fc9723c3235f24fff
GamerCleanVic <jonassd2000@gmail.com>
2023-08-25 18:52:54 -0500

    asyc_await

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,14 @@
 //Pogramação Funcional: Catch and Finally Fake API
 //https://jsonplaceholder.typicode.com/posts
 
-function clicou(){
+async function clicou(){
+    let response = await
     fetch('https://jsonplaceholder.typicode.com/posts')
-    .then((response) => {
-        console.log(response.status);
-        return response.json();
-    })
-    .then((json) => {
-        alert(`Título do 1º post: ${json[0].title}`);
-    })
-    .catch(() => {
-        alert("Opa, deu algo errado!");
-    })
-    .finally(() => {
-        alert("Terminou!");
-    })
-}
-
-function inserir(){
-    fetch('https://jsonplaceholder.typicode.com/posts',
-    {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            title: 'Título de teste',
-            body: 'Corpo teste',
-            userId: 2
-        })
-        .then((response) => {
-            return response.json();
-        })
-        .then((json) => {
-            console.log(response.status);
-            console.log(json);
-        })
-    })
+    let json = await response.json();
+    alert(`Título do 1º post: ${json[0].title}`);
+    alert("Clicou!");
+    console.log(response.status);
 }
 
 document.querySelector("#botao")
 .addEventListener('click', clicou);
-
-document.querySelector('#inserir')
-.addEventListener('click', inserir);
