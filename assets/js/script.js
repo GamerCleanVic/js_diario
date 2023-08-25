3f3c1e9901b738e48e6add50f51b8c5df61b01fa
GamerCleanVic <jonassd2000@gmail.com>
2023-08-25 18:04:43 -0500

    envioPOST

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@
 function clicou(){
     fetch('https://jsonplaceholder.typicode.com/posts')
     .then((response) => {
+        console.log(response.status);
         return response.json();
     })
     .then((json) => {
@@ -17,5 +18,30 @@ function clicou(){
     })
 }
 
+function inserir(){
+    fetch('https://jsonplaceholder.typicode.com/posts',
+    {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            title: 'Título de teste',
+            body: 'Corpo teste',
+            userId: 2
+        })
+        .then((response) => {
+            return response.json();
+        })
+        .then((json) => {
+            console.log(response.status);
+            console.log(json);
+        })
+    })
+}
+
 document.querySelector("#botao")
 .addEventListener('click', clicou);
+
+document.querySelector('#inserir')
+.addEventListener('click', inserir);
