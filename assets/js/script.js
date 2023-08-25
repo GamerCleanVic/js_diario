4f2671fcb68995df3811fb5c0710237cda274bc0
GamerCleanVic <jonassd2000@gmail.com>
2023-08-24 20:28:43 -0500

    catch e finally

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,21 @@
-//Pogramação Funcional: Callback
+//Pogramação Funcional: Catch and Finally Fake API
+//https://jsonplaceholder.typicode.com/posts
+
+function clicou(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((response) => {
+        return response.json();
+    })
+    .then((json) => {
+        alert(`Título do 1º post: ${json[0].title}`);
+    })
+    .catch(() => {
+        alert("Opa, deu algo errado!");
+    })
+    .finally(() => {
+        alert("Terminou!");
+    })
+}
 
 document.querySelector("#botao")
-    .addEventListener('click', () => {
-        alert("Botão clicado!");
-});
+.addEventListener('click', clicou);
