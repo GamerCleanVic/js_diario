a33e9792dc06e8178c9befd9963ee3838cb8db43
GamerCleanVic <jonassd2000@gmail.com>
2023-08-26 19:55:43 -0500

    data-archive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
 //Manipulando atributos
 
-function trocarImagem(filename){
+function trocarImagem(filename, animalName){
     document.querySelector('.imagem')
     .setAttribute('src', 'images/'+filename);
+    document.querySelector('.imagem')
+    .setAttribute('data-animal', animalName);
+}
+
+function pegarAnimal(){
+    let animal = document.querySelector('.imagem')
+    .getAttribute('data-animal');
+
+    alert("O animal é: "+animal);
 }
