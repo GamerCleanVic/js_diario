5adb26ab03b694d556138c9582c36f2d963fbae1
GamerCleanVic <jonassd2000@gmail.com>
2023-08-30 18:59:37 -0500

    sortReverse

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 //Metódos de Arrays 2
 
 let lista = ['Ovo', 'Farinha', 'Corante', 'Massa'];
+let lista2 = ['Prato', 'Liquidificador', 'Forma'];
 
 console.log(lista);
 lista.push('Jaca');
@@ -8,4 +9,14 @@ lista.push('Jaca');
 lista[2] = 'Leite';
 
 let res = lista;
+console.log(res);
+
+lista.splice(4, 1);
+console.log(res);
+
+res = lista.concat(lista2);
+console.log(res);
+res.sort();
+console.log(res);
+res.reverse();
 console.log(res);
