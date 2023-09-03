e108e5d16f7ea04db6961e7d182f2675266525dd
GamerCleanVic <jonassd2000@gmail.com>
2023-09-02 19:00:08 -0500

    rodar em 2s

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,11 @@
 //Intervalos
 let timer;
-function comecar(){
-    timer = setInterval(showTime, 1000);
+function rodar(){
+    timer = setTimeout(function(){
+        document.querySelector('.teste1').innerHTML = 'Rodou!';
+    }, 2000);
 }
+
 function parar(){
-    clearInterval(timer);
-}
-
-
-function showTime(){
-    let data = new Date();
-    let hora = data.getHours();
-    let minutos = data.getMinutes();
-    let segundos = data.getSeconds();
-
-    let textao = hora+" : "+minutos+" : "+segundos;
-
-    document.querySelector('.teste1').innerHTML = textao;
+    clearTimeout(timer);
 }
