bb51d640b90e836a462a1bde800db58ae4c53ee8
GamerCleanVic <jonassd2000@gmail.com>
2023-09-02 21:08:49 -0500

    tipos de função

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
-//Intervalos
-let timer;
-function rodar(){
-    timer = setTimeout(function(){
-        document.querySelector('.teste1').innerHTML = 'Rodou!';
-    }, 2000);
+function somar(x, y){
+    return `${x} + ${y} = ${x + y}`;
 }
+console.log(somar(2, 5));
 
-function parar(){
-    clearTimeout(timer);
-}
+let multi = function(x, y){
+    return `${x} + ${y} = ${x * y}`;
+};
+console.log(multi(3, 7));
+
+let divd = (x, y) =>  `${x} + ${y} = ${x / y}`;
+console.log(divd(8, 4));
+
+let letrasNome = nome => `O nome ${nome} tem ${nome.length} letras`;
+console.log(letrasNome('Jotta'));
