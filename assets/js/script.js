d29a3d889b05604d6aceabbc0375ba7f33fa6cea
GamerCleanVic <jonassd2000@gmail.com>
2023-09-04 21:39:04 -0500

    JSON_parse

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,3 @@
-function somar(x, y){
-    return `${x} + ${y} = ${x + y}`;
-}
-console.log(somar(2, 5));
+let pessoa = JSON.parse('{"nome": "João", "idade": 90}');
 
-let multi = function(x, y){
-    return `${x} + ${y} = ${x * y}`;
-};
-console.log(multi(3, 7));
-
-let divd = (x, y) =>  `${x} + ${y} = ${x / y}`;
-console.log(divd(8, 4));
-
-let letrasNome = nome => `O nome ${nome} tem ${nome.length} letras`;
-console.log(letrasNome('Jotta'));
+console.log(pessoa);
