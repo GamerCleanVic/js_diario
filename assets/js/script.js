439321784b8f79d657c89bbbd866c52010a1f905
GamerCleanVic <jonassd2000@gmail.com>
2023-09-06 20:13:58 -0500

    mostrarThumb

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,9 @@
-async function enviar(){
-    let arquivo = document.getElementById('arquivo').files[0];
+function mostrar(){
+    let imagem = document.getElementById('imagem').files[0];
 
-    let body = new FormData();
-    body.append('title', 'Img1');
-    body.append('arquivo', arquivo);
+    let img = document.createElement('img');
+    img.src = URL.createObjectURL(imagem);
+    img.width = 360;
 
-    let req = await fetch('index.html', {
-        method: 'POST',
-        body: body,
-        headers: {
-            'Content-Type': 'multipart/form-data'
-        }
-    });
+    document.getElementById('area').appendChild(img);
 }
