a1ecbc277806a5aba4477d6b2644da087e70bc94
GamerCleanVic <jonassd2000@gmail.com>
2023-09-06 19:42:34 -0500

    Enviar 1 arquivo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
-let pessoa = JSON.parse('{"nome": "João", "idade": 90}');
+async function enviar(){
+    let arquivo = document.getElementById('arquivo').files[0];
 
-console.log(pessoa);
+    let body = new FormData();
+    body.append('title', 'Img1');
+    body.append('arquivo', arquivo);
+
+    let req = await fetch('index.html', {
+        method: 'POST',
+        body: body,
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+    });
+}
