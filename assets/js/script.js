fcad85c336aea7154c8ffd0350d672e99a39e822
GamerCleanVic <jonassd2000@gmail.com>
2023-11-23 20:05:42 -0500

    actions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,28 @@
 class Person{
+    steps = 0;
+    age = 0;
+
     constructor(name, age){
         this.name = name;
         this.age = age;
     }
+
+    takeAStep(){
+        this.steps++;
+    }
+
+    setAge(newAge){
+        if(typeof newAge == 'number'){
+            this.age = newAge;
+        }else{
+            console.log("Idade não aceita, só númeors!");
+        }
+    }
 }
 
-let p1 = new Person("Jõao", 20);
-let p2 = new Person("Maria", 30);
-let p3 = new Person("Pedro", 40);
+let p1 = new Person("Jõao");
+let p2 = new Person("Maria");
+let p3 = new Person("Pedro");
 
-console.log("Nome: "+p1.name, "\nIdade: "+p1.age);
+p1.setAge('dfgdfgfd');
+console.log(`${p1.name} tem ${p1.age} anos`);
