38f8becd2af2d125a2c45e9d7946c97a85907deb
GamerCleanVic <jonassd2000@gmail.com>
2024-02-13 17:55:36 -0300

    select elements in console

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,10 @@
-//Eventos e manipulação de DOM
-//->JavaScript: Selecionar elementos2
+//Aula: Modulo 3
+//->JavaScript: Manipulando elementos 2
+
+function clicou(){
+    const teste = document.querySelector("#teste");
+    const ul = teste.querySelector("ul");
+
+    ul.children[0].innerHTML = "Item <b>alterado</b>";
+    console.log(ul.outerHTML);
+}
