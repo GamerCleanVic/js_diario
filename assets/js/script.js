30f5babe9b59973859fd0846d7c0b0212327cd04
GamerCleanVic <jonassd2000@gmail.com>
2024-05-01 17:30:17 -0300

    Manipulando Atributos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,15 @@
 //Aula: Modulo 3
-//->JavaScript: Manipulando elementos 2
+//->JavaScript: Manipulando CSS
 
 function clicou(){
-    const teste = document.querySelector("#teste");
-    const ul = teste.querySelector('ul');
+    const input = document.querySelector('input');
+    const botao = document.querySelector('.botao2');
 
-    ul.outerHTML = "<b><ul><li>UL alterada Teste</li></ul></b>";
+    if(input.getAttribute('type') === 'text'){
+        input.setAttribute('type', 'password');
+        botao.innerText = 'Mostrar senha';
+    }else{
+        input.setAttribute('type', 'text');
+        botao.innerText = 'Ocultar senha';
+    }
 }
