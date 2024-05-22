c39dc7f53ca39374e41ca16430d25eb960a0ce95
GamerCleanVic <jonassd2000@gmail.com>
2024-05-22 16:20:19 -0300

    constructor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,17 @@
 //Próxima Aula: Modulo 4
-//->JavaScript: 19 - Funcional: Instância e This
+//JavaScript: 19 - Funcional: Herança
 
-function createPerson(name, lastName, age){
-    return{
-        name,
-        lastName,
-        age
+let person = {
+    name: 'Bonieky',
+    lastName: 'Lacerda',
+    age: 18,
+    getFullName(){
+        return `${this.name} ${this.lastName}`;
+    },
+    start(){
+        console.log('Deu start na pessoa!');
     }
 }
 
-let p1Fora = createPerson('Bonieky', 'Lacerda', 18);
-let p2Fora = createPerson('Junior', 'Fulano', 20);
-
-console.log(p1Fora.name);
+person.start();
+console.log(person.getFullName());
