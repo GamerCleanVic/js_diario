0cc2cbefe9c7f2435e14ca42445e71ff87d55205
GamerCleanVic <jonassd2000@gmail.com>
2025-03-03 19:08:23 -0500

    modificar array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
 //Próxima Aula: Modulo 5
-//JavaScript: #4 - OComo uma API funciona?
+//JavaScript Módulo 2: Ordenação de array
 
+const fruits = ["Maçã", "Uva", "Laranja", "Banana"];
+
+console.log('Antes:', fruits);
+fruits[fruits.length -1] = "Pêra";
+console.log('Depois do +[0]:', fruits);
