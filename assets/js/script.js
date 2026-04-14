9a15401105ef23d0c3171213a49f9bf826bc48be
“GamerCleanVic” <jonassd2000@gmail.com>
2026-04-13 22:28:35 -0500

    change attributes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,7 @@
 //JavaScript aula : Manipulando o CSS
 
 function clicou(){
-  const input = document.querySelector('input');
-  const botao = document.querySelector('.botao');
-  
-  if(input.getAttribute('type') === 'text'){
-    input.setAttribute('type', 'password');
-    botao.innerText = 'Mostrar senha';
-  }else{
-    input.setAttribute('type', 'text');
-    botao.innerText = 'Ocultar senha';
-  }
+  const li = document.querySelector('li');
+
+  li.style.backgroundColor = '#015';
 }
